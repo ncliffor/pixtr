5af8c5cf9ab78cf8283f3195ec37bee8cf6f8d39
Nick Clifford <ncliffor@gmail.com>
2014-10-01 11:11:24 -0400

    Users can create new images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,4 +3,24 @@ class ImagesController < ApplicationController
     @gallery = Gallery.find(params[:gallery_id])
     @image = @gallery.images.find(params[:id])
   end
+
+  def new
+    @gallery = Gallery.find(params[:gallery_id])
+    @image = @gallery.images.new
+  end
+
+  def create
+    @gallery = Gallery.find(params[:gallery_id])
+    @gallery.images.create(image_params)
+
+    redirect_to (@gallery)
+  end
+
+  private
+
+  def image_params
+    params.
+    require(:image).
+      permit(:name, :url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "galleries#index"
 
   resources :galleries do
-    resources :images, only: [:show]
+    resources :images, only: [:show, :new, :create]
   end
 end
 
