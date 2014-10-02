6c3cc6bb7fe10c9208faa5930f957c23e3dd20d7
Nick Clifford <ncliffor@gmail.com>
2014-10-02 09:06:04 -0400

    Add users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "galleries#index"
 
+  resources :users, only: [:new, :create]
+
   resources :galleries do
     resources :images, only: [:show, :new, :create, :edit, :update]
   end
