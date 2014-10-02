e2136cc90ef8e864b5af103328d732f828823c44
Nick Clifford <ncliffor@gmail.com>
2014-10-02 14:37:29 -0400

    Associate galleries to users

---
+++ db/migrate/20141002181723_associate_galleries_to_users.rb
@@ -0,0 +1,15 @@
+class AssociateGalleriesToUsers < ActiveRecord::Migration
+  def up
+    add_column :galleries, :user_id, :integer
+
+    user = select_one "SELECT id FROM users LIMIT 1"
+    update "UPDATE galleries SET user_id = #{user["id"]}"
+
+    change_column_null :galleries, :user_id, false
+  end
+
+  def down
+    remove_column :galleries, :user_id
+  end
+end
+
