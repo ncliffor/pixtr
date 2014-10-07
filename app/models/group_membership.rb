400043eef6496ba531f4c2eb79bc5940dcb13949
Nick Clifford <ncliffor@gmail.com>
2014-10-07 15:29:08 -0400

    Create GroupMemberships for adding users to groups

---
+++ app/models/group_membership.rb
@@ -0,0 +1,5 @@
+class GroupMembership < ActiveRecord::Base
+  belongs_to :group
+  belongs_to :member, class_name: "User"
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :galleries
+  has_many :group_memberships
+  has_many :groups, through: :group_memberships
 
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
